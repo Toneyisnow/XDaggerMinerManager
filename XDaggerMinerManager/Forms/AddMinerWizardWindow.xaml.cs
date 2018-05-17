f516679d5867e763322d6caf6025b6c4a18461db
Toney Sui <charlie_5899@hotmail.com>
2018-05-17 16:53:44 -0700

    Checking in the Manager

---
+++ XDaggerMinerManager/Forms/AddMinerWizardWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XDaggerMinerManager.Forms
+{
+
+    /// <summary>
+    /// Interaction logic for AddMinerWizardWindow.xaml
+    /// </summary>
+    public partial class AddMinerWizardWindow : Window
+    {
+        public enum AddMinerWizardStatus
+        {
+            Initial,
+            StepOne,
+            StepTwo,
+            StepThree,
+            StepFour,
+            Finished
+        }
+
+        private AddMinerWizardStatus wizardStatus = AddMinerWizardStatus.Initial;
+
+        public AddMinerWizardWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void onClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (wizardStatus == AddMinerWizardStatus.Finished)
+            {
+                return;
+            }
+
+            MessageBoxResult result = MessageBox.Show("确定要离开此向导吗？", "确认", MessageBoxButton.YesNo);
+
+            if (result != MessageBoxResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnStepOneNext_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchUIToStep(2);
+        }
+        private void btnStepTwoNext_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchUIToStep(3);
+        }
+
+        private void btnStepThreeNext_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchUIToStep(4);
+        }
+
+        private void btnStepTwoBack_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchUIToStep(1);
+        }
+        private void btnStepThreeBack_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchUIToStep(2);
+        }
+
+        private void btnStepFourBack_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchUIToStep(3);
+        }
+
+        private void btnStepFourFinish_Click(object sender, RoutedEventArgs e)
+        {
+            wizardStatus = AddMinerWizardStatus.Finished;
+            this.Close();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="step"></param>
+        private void SwitchUIToStep(int step)
+        {
+            grdStepOne.Visibility = Visibility.Hidden;
+            grdStepTwo.Visibility = Visibility.Hidden;
+            grdStepThree.Visibility = Visibility.Hidden;
+            grdStepFour.Visibility = Visibility.Hidden;
+
+            lblStepOne.Background = null;
+            lblStepTwo.Background = null;
+            lblStepThree.Background = null;
+            lblStepFour.Background = null;
+
+            lblStepOne.FontWeight = FontWeights.Normal;
+            lblStepTwo.FontWeight = FontWeights.Normal;
+            lblStepThree.FontWeight = FontWeights.Normal;
+            lblStepFour.FontWeight = FontWeights.Normal;
+
+            switch (step)
+            {
+                case 1:
+                    grdStepOne.Visibility = Visibility.Visible;
+                    lblStepOne.Background = (SolidColorBrush)Application.Current.FindResource("WizardStepButton");
+                    lblStepOne.FontWeight = FontWeights.ExtraBold;
+                    break;
+                case 2:
+                    grdStepTwo.Visibility = Visibility.Visible;
+                    lblStepTwo.Background = (SolidColorBrush)Application.Current.FindResource("WizardStepButton");
+                    lblStepTwo.FontWeight = FontWeights.ExtraBold;
+                    break;
+                case 3:
+                    grdStepThree.Visibility = Visibility.Visible;
+                    lblStepThree.Background = (SolidColorBrush)Application.Current.FindResource("WizardStepButton");
+                    lblStepThree.FontWeight = FontWeights.ExtraBold;
+                    break;
+                case 4:
+                    grdStepFour.Visibility = Visibility.Visible;
+                    lblStepFour.Background = (SolidColorBrush)Application.Current.FindResource("WizardStepButton");
+                    lblStepFour.FontWeight = FontWeights.ExtraBold;
+                    
+                    break;
+            }
+        }
+    }
+}
